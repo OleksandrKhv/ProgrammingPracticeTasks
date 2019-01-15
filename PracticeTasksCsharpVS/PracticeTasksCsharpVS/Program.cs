34047e52ccad687a1cfac675215ccd631bcf3949
OleksandrKhv <ksalendrokon@gmail.com>
2019-01-15 20:21:11 +0200

    Added new line

--- PracticeTasksCsharpVS/PracticeTasksCsharpVS/Program.cs
+++ PracticeTasksCsharpVS/PracticeTasksCsharpVS/Program.cs
@@ -17,6 +17,7 @@ namespace PracticeTasksCsharpVS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //New line
         }
     }
 }
